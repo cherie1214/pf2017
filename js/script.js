3e4715f4b29f8c22f31e9f9fe360b2d06a346ab6
cherie1214 <cheriey1214@gmail.com>
2017-02-15 18:04:30 +0900

    2017.02.15 18:04
    마우스휠이벤트 1차

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-    new WOW().init();
+    new WOW().init(); // wow event
 
     $('.menu').mouseenter(function(){
         $('.gnb').addClass('on');
@@ -19,5 +19,86 @@ $(function(){
 
     $('.gnb a').mouseenter(function(){
         $(this).addClass('on').siblings().removeClass('on');
+    }); // menu event
+
+    var pages = [];
+    var cScrollTop = 0;
+    var index = 0;
+    var check = 0;
+
+    function movement(delta,time){
+        //이벤트 조건에 따른 에니메이션
+        if(delta > 0 ){
+            console.log('올리기');
+
+            $('body, html').animate({
+                scrollTop : pages[index-1]
+            },time,'swing');
+            check = 1;
+            setTimeout(function(){
+                playWheel();
+                check = 0;
+            },time)
+        }else if(delta < 0 ){
+            console.log('내리기');
+            $('body, html').animate({
+                scrollTop : pages[index+1]
+            },time,'swing');
+            check = 1;
+            setTimeout(function(){
+                check = 0;
+                playWheel();
+            },time)
+        }
+    }
+    function blockWheel(){
+        jQuery(window).on("mousewheel.disableScroll DOMMouseScroll.disableScroll touchmove.disableScroll", function(e) {
+            e.preventDefault();
+            return;
+        });
+        jQuery(window).on("keydown.disableScroll", function(e) {
+            var eventKeyArray = [32, 33, 34, 35, 36, 37, 38, 39, 40];
+            for (var i = 0; i < eventKeyArray.length; i++) {
+                if (e.keyCode === eventKeyArray [i]) {
+                    e.preventDefault();
+                    return;
+                }
+            }
+        });
+    }
+    function playWheel(){
+        jQuery(window).off(".disableScroll");
+    }
+
+    $('.pages').each(function(i,e){
+        pages.push($(e).offset().top);
+    });
+
+    $(window).scroll(function(){
+        cScrollTop = $(window).scrollTop();
+    });
+
+    $("html, body").on('mousewheel DOMMouseScroll', function(e) {
+        console.log(cScrollTop , pages);
+        //index 구하기
+        if(cScrollTop == pages[0]) index = 0;
+        if(cScrollTop == pages[1]) index = 1;
+
+        console.log(index)
+
+        //이벤트 확인 & 변수 통일
+        var E = e.originalEvent;
+        delta = 0;
+        if (E.detail) {
+            delta = E.detail * -40;
+        }else{
+            delta = E.wheelDelta;
+        }
+
+        // console.log(check)
+        if(check == 0 && !(index == 1 && delta < 0 )  ){
+            movement(delta,700);
+            blockWheel();
+        }
     });
 })
